f0737cad896ad00fedf7dd51304585a76565c2d4
hannashibata <hanna.y.shibata@gmail.com>
2025-05-06 13:02:39 -1000

    created a new js file to plat the hspl video on-scroll; set up css to work with the new js file; updated the hero image for hspl subpage;

---
+++ js/hspl-challenge.js
@@ -0,0 +1,52 @@
+// console.log("hello")
+
+// ON SCROLL ANIMATION FOR CHALLENGES VIDEO AND TEXT  
+const mychallengevideo = document.querySelector('#challenge-video');
+const mychallengeobserver = new IntersectionObserver((entries) => {
+
+    // look through all entries
+    entries.forEach((entry) => {
+
+    // if each is in view, play video, within timeout
+    // play video at a time set at currentTime,
+    // pause the video within 2seconds
+
+        // console.log(entry.target);
+        if (entry.isIntersecting) {
+
+            if (entry.target.classList.contains('challenge-01')) {
+
+                console.log("challenge-01 is in view");
+                mychallengevideo.currentTime=0;
+                mychallengevideo.play();
+                setTimeout(function() {
+                    mychallengevideo.pause();
+                }, 2000);
+                
+            } else if (entry.target.classList.contains('challenge-02')) {
+
+                console.log("challenge-02 is in view");
+                mychallengevideo.currentTime=2;
+                mychallengevideo.play();
+                
+            } else if (entry.target.classList.contains('challenge-03')) {
+
+                console.log("challenge-03 is in view");
+                
+            } else if (entry.target.classList.contains('challenge-04')) {
+
+                console.log("challenge-04 is in view");
+            };
+
+        };
+    });
+    
+  });
+  
+  const mychallenges = document.querySelectorAll('.observe-me-challenge');
+  mychallenges.forEach((el) => {
+    
+      // observe every target
+    mychallengeobserver.observe(el);
+    
+  });
